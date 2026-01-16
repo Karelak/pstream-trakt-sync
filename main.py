36d4e186860cd4954f695a51e0597ab68589615d
Kaarel <partkaarel@gmail.com>
2026-01-16 11:12:20 +0000

    trakt stuff works wow

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import trakt
-from dotenv import load_dotenv
 import os
-import trakt.core
 from trakt import init
+import trakt.core
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
 
-def get_trakt_token():
+def setup_trakt():
     trakt.core.AUTH_METHOD = trakt.core.OAUTH_AUTH
+    trakt.core.BASE_URL = "https://api.trakt.tv"
+    trakt.core.CONFIG_PATH = "./pytrakt.json"
     init(
-        os.getenv("TRAKT_USERNAME"),
-        store=True,
+        "karelka",
         client_id=os.getenv("TRAKT_CLIENT_ID"),
         client_secret=os.getenv("TRAKT_CLIENT_SECRET"),
+        store=True,
     )
 
 
 if __name__ == "__main__":
-    get_trakt_token()
+    setup_trakt()
